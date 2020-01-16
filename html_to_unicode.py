39fb82117c0600fc50ef95f3c46b649c1dbb1b4e
Jacques-Olivier Moussafir <msfr@mac.com>
2020-01-16 21:59:08 +0100

    add parse codex

--- html_to_unicode.py
+++ html_to_unicode.py
@@ -1,13 +1,48 @@
+import sys
 import html
 import glob
+import requests
+from urllib import request
+
+from bs4 import BeautifulSoup
 
 
-fname_list=glob.glob("articles_txt/*html")
+url_fd = open("url/url_2019-12-17.html")
+url_list=url_fd.readlines()
+url_fd.close()
 
-for f in fname_list[-1:]:
-    fd=open(f,'r')
-    lines=fd.readlines()
-    fd.close()
+url=url_list[1]
 
-    for l in lines:
-        print(html.unescape(l))
+
+
+## page = requests.get(url)
+page = request.urlopen(url).read().decode('utf8')
+
+soup = BeautifulSoup(page, 'html.parser')
+title=soup.find_all('title')
+paragraph=soup.find_all('p',  class_='article__paragraph')
+
+title = soup.title.string
+paragraph=soup.find_all('p',  class_='article__paragraph')
+
+print(title)
+
+for p in paragraph:
+    print(p.get_text())
+
+
+
+#for url in url_list[1]:
+#    page = requests.get(url)
+
+
+
+
+#title=soup.find_all('title')
+
+
+#print(title.get_text())
+
+
+
+#sys.exit(0)
